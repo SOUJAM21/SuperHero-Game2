591a41c739638767f4801ddc2c1648c3e4e61a48
SOUJAM21 <Sourabh@Sourabhs-iMac.local>
2015-04-01 22:04:31 -0700

    Fixed Camera

---
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Prefabs/MainCharacterCollision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCharacterCollision : MonoBehaviour {
+		
+	void OnCollisonEnter (Collision col)
+	{
+		if (col.gameObject.tag == "Cyclops") 
+		{
+		
+		}
+	}
+}
